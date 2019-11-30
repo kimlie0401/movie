932070d9abd0686ff2e3a79d91874a2f2210ca75
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-11-29 16:39:04 -0800

    Constants and Screens Set Up

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import TabNavigation from "./navigation/TabNavigation";
+
+export default class App extends Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      return <TabNavigation />;
+    } else {
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+        />
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
